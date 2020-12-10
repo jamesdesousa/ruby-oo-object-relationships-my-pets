7b4f3408be83c4aff67a7629f81eeae7f86f4953
James <jamesdesousa45@gmail.com>
2020-12-10 18:07:23 -0500

    my pets lab

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name, species="human")
+    @name = name
+    @species = species
+    @@all << self
+    end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+     @@all.uniq.length
+   end
+
+   def self.reset_all
+     @@all = []
+   end
+
+   def cats
+     Cat.all.select do |cats|
+       cats.owner.name == self.name
+     end
+   end
+
+   def dogs
+     arr = []
+     arr = Dog.all.select do |dogs|
+       dogs.owner.name == self.name
+
+
+     end
+       arr.sort
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
